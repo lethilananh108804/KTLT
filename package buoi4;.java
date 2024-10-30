205de0d749ac1734bb7730f169848dcc8fa4ff88
LanAnh <llelananh716443@gmail.com>
2024-10-30 15:28:53 +0700

    Add files via upload

---
+++ package buoi4;.java
@@ -0,0 +1,18 @@
+package buoi4
+
+public class bai4_2() {
+
+    String nguHanh []= {"Kim","Mộc","Thủy","Hỏa","Thổ"};
+    int cs = 0;
+    System.out.printf("Phần tử thứ nhất nguHanh[%d] = " + nguHanh[cs], cs++);
+    System.out.printf("\nPhần tử thứ hai nguHanh[%d] = " + nguHanh[cs], cs++);
+    System.out.printf("\nPhần tử thứ ba nguHanh[%d] = " + nguHanh[cs], cs++);
+    System.out.printf("\nPhần tử thứ tư nguHanh[%d] = " + nguHanh[cs], cs++);
+    System.out.printf("\nPhần tử thứ năm nguHanh[%d] = " + nguHanh[cs], cs++);
+}
+System.out.printf("Phần tử thứ nhất nguHanh[%d] = " + nguHanh[cs], cs++);System.out.printf("\nPhần tử thứ hai nguHanh[%d] = " + nguHanh[cs], cs++);System.out.printf("\nPhần tử thứ ba nguHanh[%d] = " + nguHanh[cs], cs++);System.out.printf("\nPhần tử thứ tư nguHanh[%d] = " + nguHanh[cs], cs++);System.out.printf("\nPhần tử thứ năm nguHanh[%d] = " + nguHanh[cs], cs++);
+public Static
+void main(String[] args) {
+    // TODO Auto-generated method stub
+    bai4_2();
+}
