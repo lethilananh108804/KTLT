69f75befff82a75cb246dd006a43213dbd16d851
LanAnh <llelananh716443@gmail.com>
2024-11-19 19:04:16 +0700

    Create Bai9.java

---
+++ Bai9.java/Bai9.java
@@ -0,0 +1,37 @@
+package baitapbuoi9;
+
+public class buoi9 {
+    public static void tamGiacSaoNguoc(int n)
+    {
+        System.out.println();
+        for(int i=n; i >=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }    
+    public static void main(String[] args) {
+        tamGiacSaoNguoc(7);
+    }
+}
+
+public class buoi9 {
+    public static void tamGiacSoNguoc(int n)
+    {
+        System.out.println();
+        for(int i=n; i >=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }    
+    public static void main(String[] args) {
+        tamGiacSoNguoc(7);
+    }
+}    
